5d26f120c6d7f2bccbe06a4552a85410b6e18d50
啊 <3300247992@qq.com>
2025-09-13 01:30:33 +0800

    feat(models): 新增user和task模型，并关联

--- backend/models/task.go
+++ backend/models/task.go
@@ -1,11 +1,18 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
 
-type List struct {
+	"gorm.io/gorm"
+)
+
+type Task struct {
 	gorm.Model
-	UserId      uint //外键
-	Title       string
-	Description string
-	Completed   bool
+
+	UserID string `gorm:"not null"`
+
+	Title       string    `gorm:"not null;size:100"`
+	Description string    `gorm:"size:1000"`
+	Deadline    time.Time `time_format:"2006-01-02"`
+	Completed   bool      `gorm:"default:false"`
 }
--- backend/models/user.go
+++ backend/models/user.go
@@ -5,5 +5,8 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 
-	Username string
+	Username string `gorm:"unique;not null" binding:"required"`
+	Password string `gorm:"not null;size:100" binding:"required"`
+
+	Tasks []Task `gorm:"foreignKey:UserID"`
 }
