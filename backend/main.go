15d732392f366561ee2f92a4fd069565c3592282
啊 <3300247992@qq.com>
2025-09-14 14:26:30 +0800

    feat(main): 添加退出服务器的处理

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,13 @@ import (
 	"Todolist/config"
 	"Todolist/database"
 	"Todolist/router"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -11,9 +18,35 @@ func main() {
 	config.InitConfig()
 	database.InitDB()
 
-	router := router.SetupRouter()
+	r := router.SetupRouter()
 
-	Port := config.AppConfig.App.Port
-	router.Run(Port)
+	port := config.AppConfig.App.Port
+
+	//优雅地启动和退出服务器
+	srv := &http.Server{
+		Addr:    port,
+		Handler: r.Handler(),
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	//阻塞在这里，等待系统信号
+	<-quit
+	log.Println("shut down server...")
+
+	ctx, concel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer concel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("server shutdown: %s\n", err)
+	}
+
+	log.Println("Server exiting")
 
 }
