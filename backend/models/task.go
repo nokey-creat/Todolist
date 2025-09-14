c8bd8f61bcf87f95c2255d3ec31f4654a7f3a4b2
啊 <3300247992@qq.com>
2025-09-14 14:22:24 +0800

    feat(models): 增加binding标签

--- backend/models/task.go
+++ backend/models/task.go
@@ -9,10 +9,10 @@ import (
 type Task struct {
 	gorm.Model
 
-	UserID string `gorm:"not null"`
+	UserID uint `gorm:"not null"`
 
-	Title       string    `gorm:"not null;size:100"`
-	Description string    `gorm:"size:1000"`
-	Deadline    time.Time `time_format:"2006-01-02"`
+	Title       string    `gorm:"not null;size:100" binding:"required"`
+	Description string    `gorm:"size:1000" binding:"required"`
+	Deadline    time.Time `binding:"required"`
 	Completed   bool      `gorm:"default:false"`
 }
