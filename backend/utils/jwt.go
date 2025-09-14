74b2cf4b797aa88970c45238ba9a3f504be7ea7f
啊 <3300247992@qq.com>
2025-09-14 14:24:09 +0800

    feat(utils): 添加ParseJWT函数

--- backend/utils/jwt.go
+++ backend/utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -28,7 +29,28 @@ func GenerateJWT(userId uint) (string, error) {
 	if ss, err := token.SignedString(signkey); err != nil {
 		return "", err
 	} else {
-		return "Bearer " + ss, nil
+		//return "Bearer " + ss, nil //前端会加bearer
+		return ss, nil
+	}
+
+}
+
+func ParseJWT(tokenString string) (uint, error) {
+	//解析并验证
+	token, err := jwt.ParseWithClaims(tokenString, &UserClaims{}, func(token *jwt.Token) (any, error) {
+		//该函数接收已解析但未验证的 Token
+		//这里限制签名算法
+		if token.Method.Alg() != jwt.SigningMethodHS256.Alg() {
+			return nil, errors.New("JWT with Unexpected signed method")
+		}
+		return signkey, nil
+	})
+	if err != nil {
+		return 0, err
+	} else if claims, ok := token.Claims.(*UserClaims); ok && token.Valid {
+		return claims.UserId, nil
+	} else {
+		return 0, errors.New("unknown claims type, cannot proceed")
 	}
 
 }
